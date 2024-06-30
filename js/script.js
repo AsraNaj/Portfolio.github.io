4dae1de87ed191b8f95e4f710c6380670aa61f9c
Asra Naj <asranaj344@gmail.com>
2024-06-30 22:06:26 +0530

    portfolio

---
+++ js/script.js
@@ -0,0 +1,49 @@
+$(function(){
+        $('#menu').slicknav({
+            label:"",
+            brand:"Asra Naj"
+        });
+    });
+$(document).ready(function() {
+  $(".skitter-large").skitter({
+    dots:false,
+    navigation: true,
+    theme:'clean'
+  });
+});
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:false,
+            loop:true
+        }
+    }
+  });
+});
+$(document).ready(function(){
+    $(".progress-bar-html").animate({"width":"70%"},1000,function(){
+        $(".progress-bar-css").animate({"width":"50%"},1000,function(){
+            $(".progress-bar-js").animate({"width":"40%"},1000,function(){
+                $(".progress-bar-sql").animate({"width":"70%"},1000,function(){
+                    $(".progress-bar-pythn").animate({"width":"70%"},1000,function(){
+                         $(".progress-bar-ms").animate({"width":"70%"},1000);
+                    });
+                });
+
+            });
+        });
+    });
+});
